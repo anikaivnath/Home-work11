096e62fd21cb8e4d9606ad72b9bb2f8188d1c740
anikaivnath <anikaivnath8@gmail.com>
2024-08-25 15:49:43 +0600

    Home work11

---
+++ script.js
@@ -0,0 +1,8 @@
+const step= document.getElementsByClassName('stp1');
+const txt=document.getElementsByClassName('txt');
+
+for(let i=0;i<=step.length;i++){
+    if()step[i].addEventListener('click',()=>{
+        txt[i].style.display='block';
+    })
+}
